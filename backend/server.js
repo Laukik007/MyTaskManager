612d8d447450ba09d87cb672be1164898ea1af67
Laukik <88535750+Laukik007@users.noreply.github.com>
2022-12-14 18:54:39 +0530

    updated path

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,10 @@ app.use("/api/users", userRoutes);
 // const __dirname = path.resolve();
 console.log('path = ',__dirname,path.join(__dirname, "/frontend/build"),path.resolve())
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(path.resolve(), "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(path.resolve(), "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
