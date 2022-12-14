f4e5802b79ab6f5f48889c29c9b73fc30edb38c2
Laukik <88535750+Laukik007@users.noreply.github.com>
2022-12-14 18:01:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,13 @@ app.use("/api/tasks", taskRoutes);
 app.use("/api/users", userRoutes);
 
 // --------------------------deployment------------------------------
-const __dirname = path.resolve();
+const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
